34d8cf1fd15c26874b6b8ca27b0cc3fe4a1d3d00
Florian <florian.heinrich@smail.th-koeln.de>
2021-09-19 17:29:45 +0200

    dockerized

---
+++ docker-entrypoint.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# use environment variables
+# quite dirty and PRs are sincerely welcome
+grep -Rl "https://plantuml-editor.kkeisuke.com" . | xargs -I {} sed -i "s|https://plantuml-editor.kkeisuke.com|${VUE_APP_URL%/}|g" {}
+grep -Rl "https://plantuml-server.kkeisuke.dev" . | xargs -I {} sed -i "s|https://plantuml-server.kkeisuke.dev|${VUE_APP_CDN%/}|g" {}
+
+# NGINX entrypoint
+/docker-entrypoint.sh "$@"
